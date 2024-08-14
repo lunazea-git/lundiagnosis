b6342a9cdbe2cdb5ebd4c9128f8d7ca9f1ba36dd
lunazea <112971938+lunazea-git@users.noreply.github.com>
2024-08-14 14:19:43 +0000

    first version of the program

---
+++ lundiagnosis_beta.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#required dependencies: AWK / sensors / 
+
+while [ true ]; do
+
+date_hours=$(date)
+username=$(whoami)
+kernel_version=$(uname -r)
+cpu_temp=$(sensors | grep 'Core 0' | cut -d+ -f2 | cut -d' ' -f1)
+local_ip=$(ip addr show | grep 'inet ' | grep -v '127.0.0.1' | cut -d' ' -f6)
+uptime=$(uptime -p)
+
+#checking if the user has used sudo
+
+if [ "$username" = "root" ]; then
+  echo "W: running as root, there is no reason to use this as root"
+fi
+
+echo "                  ------- $date_hours  -------"
+echo "----------------------------------------------------------------------------"
+echo
+echo "GENERAL TAB"
+echo 
+echo "PC uptime:" $uptime
+echo "user name:" $username
+echo "kernel version:" $kernel_version
+echo "local ip of this pc:" $local_ip
+echo "----------------------------------------------------------------------------"
+echo
+echo CPU temp
+echo
+echo "CPU temp:" $cpu_temp
+echo 
+echo "----------------------------------------------------------------------------"
+echo
+echo "RAM"
+echo
+echo "ram amount:" && free -ght
+echo "----------------------------------------------------------------------------"
+echo
+echo "USB devices"
+echo
+lsusb
+
+#change it to increase or decrease the refresh rate, now it is 1 which means every 1 second it updates (of course the clock will also update every selected number of seconds so the default is 1)
+sleep 1
+
+clear
+done
+
