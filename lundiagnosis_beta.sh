c7751ab17f17836dfe63c40d90bd6eb688990290
lunazea <112971938+lunazea-git@users.noreply.github.com>
2024-08-23 11:31:38 +0000

    lundiagnosis beta update v1.0
    *now reading the temperature is done by reading the file 
    
    sys/class/thermal/thermal_zone0/temp

--- lundiagnosis_beta.sh
+++ lundiagnosis_beta.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#required dependencies: AWK / sensors / 
+#required dependencies: AWK / sensors /
 
 while [ true ]; do
 
 date_hours=$(date)
 username=$(whoami)
 kernel_version=$(uname -r)
-cpu_temp=$(sensors | grep 'Core 0' | cut -d+ -f2 | cut -d' ' -f1)
+cpu_temp=$(cat /sys/class/thermal/thermal_zone0/temp | awk '{print ($1/1000)}')
 local_ip=$(ip addr show | grep 'inet ' | grep -v '127.0.0.1' | cut -d' ' -f6)
 uptime=$(uptime -p)
 
@@ -21,7 +21,7 @@ echo "                  ------- $date_hours  -------"
 echo "----------------------------------------------------------------------------"
 echo
 echo "GENERAL TAB"
-echo 
+echo
 echo "PC uptime:" $uptime
 echo "user name:" $username
 echo "kernel version:" $kernel_version
@@ -30,8 +30,8 @@ echo "--------------------------------------------------------------------------
 echo
 echo CPU temp
 echo
-echo "CPU temp:" $cpu_temp
-echo 
+echo "CPU temp:" $cpu_temp C
+echo
 echo "----------------------------------------------------------------------------"
 echo
 echo "RAM"
@@ -43,9 +43,5 @@ echo "USB devices"
 echo
 lsusb
 
-#change it to increase or decrease the refresh rate, now it is 1 which means every 1 second it updates (of course the clock will also update every selected number of seconds so the default is 1)
-sleep 1
 
-clear
-done
 
